d77dd1684aebb1078199f7cbaef19dbd9ad7e162
TLAquarius <phuocthanhgc@gmail.com>
2024-08-06 19:49:57 +0700

    thêm method tự cập nhật rule id dựa vào rule mới nhất được thêm vào cơ sở

--- models/season.py
+++ models/season.py
@@ -1,4 +1,5 @@
 from . import db
+from .rule import Rule
 
 
 class Season(db.Model):
@@ -9,3 +10,12 @@ class Season(db.Model):
     note = db.Column(db.String(255), nullable=True)
 
     rules = db.relationship('Rule', backref=db.backref('seasons', lazy=True))
+
+    def update_latest_rule_id(self):
+        latest_rule = Rule.query.order_by(Rule.id.desc()).first()
+        if latest_rule:
+            self.rule_id = latest_rule.id
+        else:
+            Rule.insert_default_rule()
+            latest_rule = Rule.query.order_by(Rule.id.desc()).first()
+            self.rule_id = latest_rule.id
