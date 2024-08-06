8f3336b23f00296171c7fca4d230a1725e9180e2
TLAquarius <phuocthanhgc@gmail.com>
2024-08-07 00:51:24 +0700

    fix không nhận input sửa rule

--- routes/rule_routes.py
+++ routes/rule_routes.py
@@ -6,31 +6,31 @@ def setup_rule_routes(app):
     def rule_change():
         if request.method == 'POST':
             # Get data from the form
-            min_players = request.form.get('min_players')
-            max_players = request.form.get('max_players')
-            min_age = request.form.get('min_age')
-            max_age = request.form.get('max_age')
-            max_foreign_players = request.form.get('max_foreign_players')
-            max_score_type = request.form.get('max_score_type')
-            min_match_time = request.form.get('min_match_time')
-            max_match_time = request.form.get('max_match_time')
-            win_points = request.form.get('win_points')
-            draw_points = request.form.get('draw_points')
-            lose_points = request.form.get('lose_points')
+            min_players = request.form.get('minimum_players')
+            max_players = request.form.get('maximum_players')
+            min_age = request.form.get('minimum_age')
+            max_age = request.form.get('maximum_age')
+            max_foreign_players = request.form.get('maximum_foreign_players')
+            max_score_type = request.form.get('maximum_score_type')
+            min_match_time = request.form.get('minimum_score_time')
+            max_match_time = request.form.get('maximum_score_time')
+            win_points = request.form.get('win_point')
+            draw_points = request.form.get('draw_point')
+            lose_points = request.form.get('lose_point')
 
             # Create a new rule
             new_rule = Rule(
-                min_players=min_players,
-                max_players=max_players,
-                min_age=min_age,
-                max_age=max_age,
-                max_foreign_players=max_foreign_players,
-                max_score_type=max_score_type,
-                min_match_time=min_match_time,
-                max_match_time=max_match_time,
-                win_points=win_points,
-                draw_points=draw_points,
-                lose_points=lose_points
+                minimum_players=min_players,
+                maximum_players=max_players,
+                minimum_age=min_age,
+                maximum_age=max_age,
+                maximum_foreign_players=max_foreign_players,
+                maximum_score_type=max_score_type,
+                minimum_score_time=min_match_time,
+                maximum_score_time=max_match_time,
+                win_point=win_points,
+                draw_point=draw_points,
+                lose_point=lose_points
             )
 
             db.session.add(new_rule)
