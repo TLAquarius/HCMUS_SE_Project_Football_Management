5ff6550032aa349c3c8f86ae9f8de03e8bbe5504
nahoang1104 <nahoang22@clc.fitus.edu.vn>
2024-08-15 14:11:14 +0700

    test

--- models/team.py
+++ models/team.py
@@ -1,5 +1,5 @@
 from . import db
-
+from models import Player
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +11,19 @@ class Team(db.Model):
 
     # Relationship with Season
     season = db.relationship('Season', backref=db.backref('teams', cascade='all, delete-orphan', lazy=True))
+
+    def count_native(self):
+        players = Player.query.filter_by(team_id = self.id).all()
+        native = 0
+        for player in players:
+            if player.player_type == 'native':
+                native += 1
+        return native
+    
+    def count_foreign(self):
+        players = Player.query.filter_by(team_id = self.id).all()
+        foreign = 0
+        for player in players:
+            if player.player_type == 'foreign':
+                foreign += 1
+        return foreign
