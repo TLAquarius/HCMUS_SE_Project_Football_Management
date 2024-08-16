65888138765f94af319c95e5068fc63e5edc47e1
nahoang1104 <nahoang22@clc.fitus.edu.vn>
2024-08-16 17:20:13 +0700

    sửa query team ranking và match

--- routes/season_routes.py
+++ routes/season_routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from models import db, Season, Team, Player, Match, MatchResult, TeamRanking
 import os
 from werkzeug.utils import secure_filename
+from datetime import datetime
 
 def setup_season_routes(app):
     @app.route('/season/<int:season_id>')
@@ -9,9 +10,9 @@ def setup_season_routes(app):
         season = Season.query.get_or_404(season_id)
         teams = Team.query.filter_by(season_id=season_id).all()
         players = Player.query.filter_by(season_id=season_id).all()
-        matches = Match.query.filter_by(season_id=season_id).all()
+        matches = Match.query.join(Match.host_team).filter(Team.season_id == season_id, Match.match_datetime < datetime.utcnow()).order_by(Match.match_datetime.desc()).all()
         match_results = MatchResult.query.filter_by(season_id=season_id).all()
-        team_rankings = TeamRanking.query.filter_by(season_id=season_id).all()
+        team_rankings = TeamRanking.query.join(Team).filter(Team.season_id == season_id).order_by(TeamRanking.ranking).all()
         return render_template('season_main.html', season=season, teams=teams, players=players, matches=matches, match_results=match_results, team_rankings=team_rankings)
     @app.route('/add-season', methods=['GET', 'POST'])
     def add_season():
